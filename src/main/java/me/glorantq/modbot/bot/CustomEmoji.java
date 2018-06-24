4d6b0dfd13df2193cda01b09e2e586f2ebf2250c
Gerber Lóránt Viktor <glorantq@gmail.com>
2018-06-25 01:36:43 +0200

    New Emoji

--- src/main/java/me/glorantq/modbot/bot/CustomEmoji.java
+++ src/main/java/me/glorantq/modbot/bot/CustomEmoji.java
@@ -4,5 +4,7 @@ public class CustomEmoji {
     public static final String X = "<:crossmark_274c:458041446059081729>",
                                CHECK_MARK = "<:heavycheckmark_2714:458041446201819146>",
                                LINK = "<:linksymbol_1f517:460561702824574986>",
-                               STAR = "<:whitemediumstar_2b50:460580409177014275>";
+                               STAR = "<:whitemediumstar_2b50:460580409177014275>",
+                               BLACK_STAR = "<:blackstar:460584758423060481>",
+                               SPEECH_BALLOON = "<:speechballoon_1f4ac:460588907243765760>";
 }
--- src/main/java/me/glorantq/modbot/bot/GoogleEmoji.java
+++ src/main/java/me/glorantq/modbot/bot/GoogleEmoji.java
@@ -5,7 +5,7 @@ public class GoogleEmoji {
                          X = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/cross-mark_274c.png",
                          GEAR = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/gear_2699.png",
                          WHITE_QUESTION = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/white-question-mark-ornament_2754.png",
-                         SPEECH_BALOON = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/speech-balloon_1f4ac.png",
+                         SPEECH_BALLOON = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/speech-balloon_1f4ac.png",
                          CARD_INDEX = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/card-index_1f4c7.png",
                          CHECK_MARK = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/heavy-check-mark_2714.png",
                          LINK = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/link-symbol_1f517.png",
