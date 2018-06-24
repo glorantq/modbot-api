ac0f6d9955ea9e68d6fe1f82d690322a0bc14d47
Gerber Lóránt Viktor <glorantq@gmail.com>
2018-06-24 22:32:23 +0200

    Refactor Emojis

---
+++ src/main/java/me/glorantq/modbot/bot/CustomEmoji.java
@@ -0,0 +1,6 @@
+package me.glorantq.modbot.bot;
+
+public class CustomEmoji {
+    public static final String X = "<:crossmark_274c:458041446059081729>",
+                               CHECK_MARK = "<:heavycheckmark_2714:458041446201819146>";
+}
---
+++ src/main/java/me/glorantq/modbot/bot/GoogleEmoji.java
@@ -0,0 +1,10 @@
+package me.glorantq.modbot.bot;
+
+public class GoogleEmoji {
+    public static final String X = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/cross-mark_274c.png",
+                         GEAR = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/gear_2699.png",
+                         WHITE_QUESTION = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/white-question-mark-ornament_2754.png",
+                         SPEECH_BALOON = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/speech-balloon_1f4ac.png",
+                         CARD_INDEX = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/card-index_1f4c7.png",
+                         CHECK_MARK = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/heavy-check-mark_2714.png";
+}
