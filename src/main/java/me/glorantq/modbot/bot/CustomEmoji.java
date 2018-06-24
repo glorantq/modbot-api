1b06e87917e08a076e765ec32dac0eb3ebd73671
Gerber Lóránt Viktor <glorantq@gmail.com>
2018-06-25 01:04:00 +0200

    New Emoji

--- src/main/java/me/glorantq/modbot/bot/CustomEmoji.java
+++ src/main/java/me/glorantq/modbot/bot/CustomEmoji.java
@@ -3,5 +3,6 @@ package me.glorantq.modbot.bot;
 public class CustomEmoji {
     public static final String X = "<:crossmark_274c:458041446059081729>",
                                CHECK_MARK = "<:heavycheckmark_2714:458041446201819146>",
-                               LINK = "<:linksymbol_1f517:460561702824574986>";
+                               LINK = "<:linksymbol_1f517:460561702824574986>",
+                               STAR = "<:whitemediumstar_2b50:460580409177014275>";
 }
