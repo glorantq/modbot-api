51a56fe1cc6fc580ecb6fd78c23257b603af747d
Gerber Lóránt Viktor <glorantq@gmail.com>
2018-06-25 01:04:31 +0200

    New Emoji

--- src/main/java/me/glorantq/modbot/bot/GoogleEmoji.java
+++ src/main/java/me/glorantq/modbot/bot/GoogleEmoji.java
@@ -1,7 +1,8 @@
 package me.glorantq.modbot.bot;
 
 public class GoogleEmoji {
-    public static final String X = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/cross-mark_274c.png",
+    public static final String
+                         X = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/cross-mark_274c.png",
                          GEAR = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/gear_2699.png",
                          WHITE_QUESTION = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/white-question-mark-ornament_2754.png",
                          SPEECH_BALOON = "https://emojipedia-us.s3.amazonaws.com/thumbs/240/google/119/speech-balloon_1f4ac.png",
